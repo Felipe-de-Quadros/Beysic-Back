4b141cffb95c73b38af390a5bebbadf432aac010
talesagr <tales.aguiar@tech4log.com>
2024-09-06 16:07:25 -0300

    alterando banco de dados para cloud

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,11 @@ import { User } from "./models/User";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
-  username: "tales",
-  password: "Masterkey",
-  database: "beysic",
+  username: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
   entities: [Ticket, Order, Payment, User],
   migrations: ["src/migrations/*.ts"],
   synchronize: false,
